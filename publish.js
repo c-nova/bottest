7a5f2335c0652cb08edeacca26e51b54186d1804
Masayoshi Otsuka <maotsuka@microsoft.com>
2018-02-21 17:38:14 +0900

    Initial Release

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../bottestmo.zip');
+var kuduApi = 'https://bottestmo.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$bottestmo';
+var password = 'HzjrxaNxeY0lWez6aeXgLbdfQ20fwx8W9XDPdYiFzftDDTeC0LRxCavDbNbY';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('bottestmo publish');
+  } else {
+    console.error('failed to publish bottestmo', err);
+  }
+});
